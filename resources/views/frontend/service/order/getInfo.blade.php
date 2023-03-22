0909b93d58ce28debbffe65284943887b6d2935f
Nurul Komor <58949982+nurul-komor@users.noreply.github.com>
2023-03-22 13:00:54 +0600

    order place  section completed

--- resources/views/frontend/service/order/getInfo.blade.php
+++ resources/views/frontend/service/order/getInfo.blade.php
@@ -112,15 +112,16 @@
                                 </small>
                             </div>
                             <br>
+                            <input type="hidden" name="serviceId" value="{{ $data['service_id'] }}">
+                            <input type="hidden" name="quality" value="{{ $data['quality'] }}">
+                            <input type="hidden" name="promoCode" value="{{ $data['promoCode'] }}">
                             <div id="button-here">
                                 <button style="border: 2px solid #2B40AF" type="submit"
                                     class="float-right transition duration-150 ease-in text-blue-700 hover:text-white  border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">Submit</button>
                             </div>
                         </div>
                     </div>
-                    <input type="hidden" name="serviceId" value="{{ $data['service_id'] }}">
-                    <input type="hidden" name="quality" value="{{ $data['quality'] }}">
-                    <input type="hidden" name="promoCode" value="{{ $data['promoCode'] }}">
+
                 </form>
             </div>
             <br>
@@ -160,7 +161,24 @@
 
                     if (response.status == 1) {
                         $('#button-here').hide()
-                        alert(response.data)
+                        $("#get-info")[0].reset();
+                        $("#get-info").html(`
+                            <div id="alert-additional-content-3" class="p-4 mb-4 text-green-800 border border-green-300 rounded-lg bg-green-300  " role="alert">
+                            <div class="flex items-center">
+                                <svg aria-hidden="true" class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                                <span class="sr-only">Info</span>
+                                <h3 class="text-lg font-medium">Success</h3>
+                            </div>
+                            <div class="mt-2 mb-4 text-sm ">
+                                ${response.data}
+                            </div>
+                            <div class="flex">
+                            <a href="/" class="text-white bg-green-800 hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                                Home
+                                </a>
+                            </div>
+                            </div>
+                        `)
                     } else {
                         $('#button-here').html(`
                             <button style="border: 2px solid #2B40AF" type="submit"
